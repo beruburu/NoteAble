1fa59706f30c39752bb0daae4b4f07b7c33c75bc
Ariana Farkhondeh <a.rian.a@hotmail.com>
2016-05-19 10:58:08 -0700

    menu stuff

--- js/mainmenu.js
+++ js/mainmenu.js
@@ -13,6 +13,7 @@ $(document).ready(function () {
             expanded = true;
 
         } else {
+            $("#novicecontent").slideToggle();
             div.animate({ height: '15%' }, "slow");
             $("#intermediate").animate({ top: '37%' });
             $("#advanced").animate({ top: '64%' });
@@ -35,9 +36,32 @@ $(document).ready(function () {
             expanded = true;
        
         } else {
+            $("#intermediatecontent").slideToggle();
             div.animate({ height: '15%' }, "slow");
-            $("#novice").animate({ top: '37%' });
+            $("#novice").animate({ top: '10%' });
             $("#advanced").animate({ top: '64%' });
+            expanded = false;
+        }
+    });
+});
+
+//Expands advanced stage to reveal two levels when clicked
+$(document).ready(function () {
+    $("#advanced").click(function () {
+        var div = $("#advanced");
+
+        if (!expanded) {
+            $("div").animate({top: '0%'});
+            div.animate({ height: '64%' }, "slow");
+            $("#intermediate").animate({ top: '100%' });
+            $("#novice").animate({ top: '100%' });
+            $("#advancedcontent").slideToggle();           
+            expanded = true;
+
+        } else {
+            div.animate({ height: '15%' }, "slow");
+            $("#intermediate").animate({ top: '37%' });
+            $("#novice").animate({ top: '10%' });
             expanded = false;
         }
     });
