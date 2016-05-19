646dd243caf11bed3ca9473fa9a5d6941a34a777
Ariana Farkhondeh <a.rian.a@hotmail.com>
2016-05-19 15:31:14 -0700

    MENU WORKS NOW PRAISE THE LORD

--- js/mainmenu.js
+++ js/mainmenu.js
@@ -7,15 +7,18 @@ $(document).ready(function () {
 
         if (!expanded) {
             div.animate({ height: '64%' }, "slow");
+            $("#noviceword").animate({ top: '8.75%' });
+            $("#novicenote").animate({ top: '6.25%' });
             $("#intermediate").animate({ top: '100%' });
             $("#advanced").animate({ top: '100%' });
             $("#novicecontent").slideToggle();           
             expanded = true;
 
         } else {
-            $("#novicecontent").slideToggle();
             div.animate({ height: '15%' }, "slow");
-            $("#intermediate").animate({ top: '37%' });
+            $("#noviceword").animate({ top: '37%' });
+            $("#novicenote").animate({ top: '26%' });
+            $("#intermediate").animate({ top: '22%' });
             $("#advanced").animate({ top: '64%' });
 			$("#novicecontent").slideToggle();
             expanded = false;
@@ -29,16 +32,21 @@ $(document).ready(function () {
         var div = $("#intermediate");
 
         if (!expanded) {
-			div.animate({ top: '10%'}, "slow")
-            div.animate({ height: '56%' }, "slow");
+            div.animate({ top: '-5%' }, "slow");
+            div.animate({ height: '64%' }, "slow");
+            $("#intermediateword").animate({ top: '10%' });
+            $("#intermediatenote").animate({ top: '8%' });
             $("#novice").animate({ top: '100%' });
             $("#advanced").animate({ top: '100%' });
-            $("#intermediatecontent").slideToggle(); 
+            $("#intermediatecontent").slideToggle();
             expanded = true;
-       
+
         } else {
             $("#intermediatecontent").slideToggle();
             div.animate({ height: '15%' }, "slow");
+            div.animate({ top: '22%' }, "slow");            
+            $("#intermediateword").animate({ top: '38.5%' });
+            $("#intermediatenote").animate({ top: '30%' });
             $("#novice").animate({ top: '10%' });
             $("#advanced").animate({ top: '64%' });
             expanded = false;
@@ -52,17 +60,24 @@ $(document).ready(function () {
         var div = $("#advanced");
 
         if (!expanded) {
-            $("div").animate({top: '0%'});
+            div.animate({ top: '10%' }, "slow");
             div.animate({ height: '64%' }, "slow");
+            $("#advancedword").animate({ top: '10%' });
+            $("#advancednote").animate({ top: '8%' });
             $("#intermediate").animate({ top: '100%' });
             $("#novice").animate({ top: '100%' });
-            $("#advancedcontent").slideToggle();           
+            $("#advancedcontent").slideToggle();
             expanded = true;
 
         } else {
+            $("#advancedcontent").slideToggle();
             div.animate({ height: '15%' }, "slow");
-            $("#intermediate").animate({ top: '37%' });
+            div.animate({ top: '64%' }, "slow");
+            $("#advancedword").animate({ top: '38.5%' });
+            $("#advancednote").animate({ top: '24%' });
+            $("#intermediate").animate({ top: '22%' });
             $("#novice").animate({ top: '10%' });
+
             expanded = false;
         }
     });
